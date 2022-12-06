0099c83ae1ab50dfcc8ddfb22b515deaf41666f5
Sebastian Pastorenzi <102265528+seba-pas@users.noreply.github.com>
2022-12-06 14:59:48 -0300

    un poco de web3

---
+++ config/web3/index.js
@@ -0,0 +1,13 @@
+import web3 from "web3";
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+const ETHEREUM_WORKING_ID = 1;
+
+export const connector = new InjectedConnector({
+  supportedChainIds: [ETHEREUM_WORKING_ID],
+});
+
+export const getLibrary = (provider) => {
+  const library = new web3(provider);
+  return library;
+};
